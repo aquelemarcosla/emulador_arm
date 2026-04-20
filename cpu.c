d8aa0e741dce2f444f0a90bc227908b8c53af17b
Marcos Aurelio <83597143+aquelemarcosla@users.noreply.github.com>
2026-04-20 20:01:54 -0300

    .

--- cpu.c
+++ cpu.c
@@ -2,10 +2,16 @@
 #include "stdint.h"
 
 void reg_write(CPU *cpu, int reg, uint64_t value) {
+    if (reg == 31) {
+        return;
+    }
     cpu->regs[reg] = value;
 }
 
 uint64_t reg_read(CPU *cpu, int reg) {
+    if (reg == 31) {
+        return 0;
+    }
     return cpu->regs[reg];
 }
 
