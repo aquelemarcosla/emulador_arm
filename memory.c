bd3d53379ef7da53a468250bc12b50298e7e50a0
Marcos Aurelio <83597143+aquelemarcosla@users.noreply.github.com>
2026-04-23 00:37:26 -0300

    ..

--- memory.c
+++ memory.c
@@ -10,7 +10,7 @@ uint64_t mem_read(uint64_t address) {
         // limite
     }
 
-    for (int i = 0; i <= 7; i++) {
+    for (int i = 0; i <= 7; i++) {  
         result |= (uint64_t)memory[address + i] << (i * 8); // cast para 64 bits antes do shift para não estourar o dado
     }
     return result;
