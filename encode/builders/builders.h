fbf258913e4fea20183427215fc373f6d77d2e46
Marcos <marcosaurelio060204@gmail.com>
2026-06-05 11:01:56 -0300

    Limpeza CMAKE files

--- encode/builders/builders.h
+++ encode/builders/builders.h
@@ -1,7 +1,7 @@
 #ifndef BUILDERS_H
 #define BUILDERS_H
 
-// Incluí todos os builders.
+//DPI
 #include "builderDPI/dpi_Arithmetic/builderDPI_A.h"
 #include "builderDPI/dpi_Logic/builderDPI_L.h"
 #include "builderDPI/dpi_Move/builderDPI_MOV.h"
